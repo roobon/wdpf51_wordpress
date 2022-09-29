6448594d0b90ce8ba7697d9de3a5be5a56b5af88
roobon <roobon@gmail.com>
2022-09-29 19:06:42 +0600

    TokeyTheme Development-2nd day

---
+++ wp_project3/wp-content/themes/tokeytheme/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Purple Blog</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+</head>
+<body>
+<div id="container">
+  <div id="logo">
+    <h1><a href="<?php echo get_home_url(); ?>">Purple</a></h1>
+    <h2 id="tagline">By Henry Jorge, and TemplateFusion.</h2>
+  </div>
+  <div id="menu"> <a href="#" id="login">login</a> <a href="#" id="rss-entries">rss entries</a> <a href="#" id="rss-comments">rss comments</a> </div>
+  <div class="clearing">&nbsp;</div>
+  <ul id="nav">
+    <li><a href="#">&nbsp;&nbsp;Home&nbsp;</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Services</a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Henry</a></li>
+    <li><a href="#">Tf</a></li>
+  </ul>
+  <div id="search">
+    <form method="get" id="searchform" action="#">
+      <div id="s-text">
+        <input value="Search" name="s" id="s" type="text" />
+        <label for="s">Search</label>
+      </div>
+      <div id="s-submit">
+        <input id="searchsubmit" value="" type="submit" />
+      </div>
+    </form>
+  </div>
