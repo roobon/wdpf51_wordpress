722a054d683b2cbdcdda34c74fdf154babb519d2
roobon <roobon@gmail.com>
2022-10-04 19:04:48 +0600

    zboommusic theme updates

---
+++ wp_project3/wp-content/themes/zBoomMusic/functions.php
@@ -0,0 +1,42 @@
+<?php 
+
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'widgets' );
+
+function zboom_register_nav_menu(){
+    register_nav_menus( array(
+        'primary_menu' => __( 'Primary Menu', 'zboommusic'),
+        'footer_menu'  => __( 'Footer Menu', 'zboommusic'),
+    ) );
+}
+
+add_action( 'after_setup_theme', 'zboom_register_nav_menu');
+
+function zboom_widgets_init() {
+     register_sidebar( array(
+        'name' => __( 'Right Sidebar', 'zboom' ),
+        'id' => 'sidebar-1',
+        'description' => __( 'The main sidebar appears on the right on each page except the front page template', 'zboom' ),
+        'before_widget' => '<div class="col-1-3"><div class="wrap-col">',
+        'after_widget' => '</div></div></div></div>',
+        'before_title' => '<div class="box"><div class="heading"><h2>',
+        'after_title' => '</h2></div><div class="content">',
+    ) );
+     register_sidebar( array(
+        'name' =>__( 'Footer sidebar', 'zboom'),
+        'id' => 'sidebar-2',
+        'description' => __( 'Appears on the static front page template', 'zboom' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+    }
+ 
+add_action( 'widgets_init', 'zboom_widgets_init' );
+
+
+// Disables the block editor from managing widgets in the Gutenberg plugin.
+add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
+// Disables the block editor from managing widgets.
+add_filter( 'use_widgets_block_editor', '__return_false' );
--- wp_project3/wp-content/themes/zBoomMusic/page.php
+++ wp_project3/wp-content/themes/zBoomMusic/page.php
@@ -14,20 +14,13 @@
 						<?php the_post_thumbnail(); ?>
 						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 						<div class="info">[By <?php the_author(); ?> on <?php the_time('F j, Y'); ?> with <a href="<?php the_permalink(); ?>"><?php comments_number( 'no comments', '1 comment', '% comments' ); ?></a>]</div>
-						<?php the_excerpt(); ?>
+						<?php the_content(); ?>
 					</article>
 					<?php 	
 						endwhile;
 						endif;	
 					?>
 					
-					<ul id="pagi">
-						<li><a class="current" href="#">1</a></li>
-						<li><a href="#">2</a></li>
-						<li><a href="#">3</a></li>
-						<li><a href="#">4</a></li>
-						<li><a href="#">next</a></li>
-					</ul>
 				</div>
 			</div>
 			<?php get_sidebar(); ?>
