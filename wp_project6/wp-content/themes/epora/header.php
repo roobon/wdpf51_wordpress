529fe1abced609697b06eeb772756e1eaa4fb0fb
roobon <roobon@gmail.com>
2022-10-14 18:26:05 +0600

    Update header.php

--- wp_project6/wp-content/themes/epora/header.php
+++ wp_project6/wp-content/themes/epora/header.php
@@ -12,18 +12,7 @@
    <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri() ?>/assets/img/favicon.png">
 
    <!-- CSS here -->
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/bootstrap.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/meanmenu.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/animate.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/slick.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/backtotop.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/magnific-popup.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/nice-select.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/ui-icon.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/elegentfonts.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/font-awesome-pro.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/spacing.css">
-   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/style.css">
+   
    <?php wp_head(); ?>
 </head>
 
