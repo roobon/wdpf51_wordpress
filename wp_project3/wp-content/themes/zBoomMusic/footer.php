f956592670afcb2edcd64305461b4395fd020f8f
roobon <roobon@gmail.com>
2022-10-18 16:46:16 +0600

    Added news ticker

--- wp_project3/wp-content/themes/zBoomMusic/footer.php
+++ wp_project3/wp-content/themes/zBoomMusic/footer.php
@@ -6,8 +6,14 @@
 	</div>		
 </footer>
 <div class="row copyright">
-			<p>Copyright © 2013 - <a href="https://www.zerotheme.com/432/free-responsive-html5-css3-website-templates.html" target="_blank">Free Html5 Templates</a> by <a href="https://www.zerotheme.com" target="_blank">Zerotheme.com</a></p>
+			<p>Copyright © 2022 - <a href="https://www.zerotheme.com/432/free-responsive-html5-css3-website-templates.html" target="_blank">Free Html5 Templates</a> by <a href="https://www.zerotheme.com" target="_blank">Zerotheme.com</a></p>
 </div>
-		<?php wp_footer(); ?>
+<script src="<?php echo get_template_directory_uri() ?>/js/eocjs-newsticker.js"></script>
+<script>
+		$('#ticker').eocjsNewsticker({
+			speed: 25
+		});
+</script>		
+<?php wp_footer(); ?>
 </body>
 </html>
